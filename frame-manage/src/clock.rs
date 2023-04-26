92d8e97a89cd021ebafe458491871ebf51543cc0
Emily <1207666367@qq.com>
2023-04-26 16:50:55 +0800

    refine clock and clock improve;

--- frame-manage/src/clock.rs
+++ frame-manage/src/clock.rs
@@ -4,12 +4,15 @@ use core::option::Option;
 use crate::ACCESS_FLAG;
 use crate::plugins::{Manage, handle_local_pagefault};
 use crate::frame_allocator::FrameTracker;
-use kernel_vm::{AddressSpace, VmMeta, PageManager, VPN, Pte};
+use alloc::vec::Vec;
+use alloc::vec;
+use kernel_vm::{AddressSpace, VmMeta, PageManager, VPN, Pte, PPN};
 use alloc::collections::VecDeque;
 
 pub struct ClockManager<Meta: VmMeta, M: PageManager<Meta>> {
     queue: VecDeque<(VPN<Meta>, FrameTracker)>,
     ptr: usize,
+    task_id: usize,
     dummy: PhantomData<M>
 }
 
@@ -31,24 +34,32 @@ impl<Meta: VmMeta, M: PageManager<Meta>> ClockManager<Meta, M> {
 
 impl<Meta: VmMeta, M: PageManager<Meta>> Clone for ClockManager<Meta, M> {
     fn clone(&self) -> Self {
-        Self { queue: self.queue.clone(), ptr: self.ptr, dummy: PhantomData }
+        Self { queue: self.queue.clone(), ptr: self.ptr, task_id: usize::MAX, dummy: PhantomData }
     }
 }
 
-impl<Meta: VmMeta, M: PageManager<Meta>> Manage<Meta, M> for ClockManager<Meta, M> {
+impl<Meta: VmMeta, M: PageManager<Meta> + 'static> Manage<Meta, M> for ClockManager<Meta, M> {
     fn new() -> Self {
-        Self { queue: VecDeque::new(), ptr: 0, dummy: PhantomData }
+        Self { queue: VecDeque::new(), ptr: 0, task_id: usize::MAX, dummy: PhantomData }
     }
 
-    fn handle_pagefault(&mut self, memory_set: &mut AddressSpace<Meta, M>, vpn: VPN<Meta>, task_id: usize) {
-        handle_local_pagefault(memory_set, vpn, task_id, self)
+    fn handle_pagefault<F>(&mut self, get_memory_set: &F, vpn: VPN<Meta>, task_id: usize)
+            where F: Fn(usize) -> &'static mut AddressSpace<Meta, M> {
+        handle_local_pagefault(get_memory_set, vpn, task_id, self)
     }
 
-    fn insert_frame(&mut self, vpn: VPN<Meta>, frame: FrameTracker) {
+    fn insert_frame(&mut self, vpn: VPN<Meta>, ppn: PPN<Meta>, task_id: usize, frame: FrameTracker) {
+        if self.task_id == usize::MAX {
+            self.task_id = task_id;
+        }
+        assert!(self.task_id == task_id);
+
         self.queue.push_back((vpn, frame))
     }
 
-    fn get_next_frame(&mut self, memory_set: &mut AddressSpace<Meta, M>) -> Option<VPN<Meta>> {
+    fn work<F>(&mut self, get_memory_set: &F) -> Vec<(PPN<Meta>, VPN<Meta>, usize)>  
+        where F: Fn(usize) -> &'static mut AddressSpace<Meta, M> {
+        let memory_set = get_memory_set(self.task_id);
         assert!(self.queue.len() != 0);
         loop {
             if self.ptr >= self.queue.len() {
@@ -60,8 +71,10 @@ impl<Meta: VmMeta, M: PageManager<Meta>> Manage<Meta, M> for ClockManager<Meta, 
                 self.ptr += 1;
             } else {
                 let (vpn, _) = self.queue.remove(self.ptr).unwrap();
-                return Some(vpn);
+                return vec![(PPN::new(0), vpn, 0)];
             }
         }
     }
+
+    fn clear_frames(&mut self, task_id: usize) {}
 }
