a448fdb47e7442371f75931cf6001b714af22763
Emily <1207666367@qq.com>
2023-05-07 16:27:32 +0800

    refine clock-improve

--- frame-manage/src/clock_improve.rs
+++ frame-manage/src/clock_improve.rs
@@ -1,84 +1,53 @@
 use core::marker::PhantomData;
-use core::option::Option;
-
-use crate::{ACCESS_FLAG, DIRTY_FLAG};
-use crate::plugins::{Manage, handle_local_pagefault};
-use crate::frame_allocator::FrameTracker;
+use crate::clock_queue::ClockQueue;
+use crate::plugins::{Manage, handle_global_pagefault};
+use crate::frame_allocator::{FrameTracker, frame_check};
 use alloc::vec::Vec;
 use alloc::vec;
-use kernel_vm::{AddressSpace, VmMeta, PageManager, VPN, Pte, PPN};
-use alloc::collections::VecDeque;
+use kernel_vm::{AddressSpace, VmMeta, PageManager, VPN, PPN};
+use alloc::collections::BTreeMap;
 
 pub struct ClockImproveManager<Meta: VmMeta, M: PageManager<Meta>> {
-    queue: VecDeque<(VPN<Meta>, FrameTracker)>,
-    ptr: usize,
-    task_id: usize,
+    queue: BTreeMap<usize, ClockQueue<Meta>>,
     dummy: PhantomData<M>
-}
-
-impl<Meta: VmMeta, M: PageManager<Meta>> ClockImproveManager<Meta, M> {
-    fn get_pte(memory_set: &mut AddressSpace<Meta, M>, vpn: &VPN<Meta>) -> Option<Pte<Meta>> {
-        memory_set.translate_to_pte(vpn.base())
-    }
-
-    fn get_accessed_dirty(memory_set: &mut AddressSpace<Meta, M>, vpn: &VPN<Meta>) -> (bool, bool) {
-        let pte = Self::get_pte(memory_set, vpn).unwrap();
-        let flags = pte.flags().val();
-        ((flags & ACCESS_FLAG) != 0, (flags & DIRTY_FLAG) != 0)
-    }
-
-    fn clear_accessed_and_dirty(memory_set: &mut AddressSpace<Meta, M>, vpn: &VPN<Meta>) {
-        memory_set.clear_accessed_and_dirty(*vpn);
-    }
-}
-
-impl<Meta: VmMeta, M: PageManager<Meta>> Clone for ClockImproveManager<Meta, M> {
-    fn clone(&self) -> Self {
-        Self { queue: self.queue.clone(), ptr: self.ptr, task_id: usize::MAX, dummy: PhantomData }
-    }
 }
 
 impl<Meta: VmMeta, M: PageManager<Meta> + 'static> Manage<Meta, M> for ClockImproveManager<Meta, M> {
     fn new() -> Self {
-        Self { queue: VecDeque::new(), ptr: 0, task_id: usize::MAX, dummy: PhantomData }
+        Self { queue: BTreeMap::new(), dummy: PhantomData }
     }
 
     fn handle_pagefault<F>(&mut self, get_memory_set: &F, vpn: VPN<Meta>, task_id: usize)
             where F: Fn(usize) -> &'static mut AddressSpace<Meta, M> {
-        handle_local_pagefault(get_memory_set, vpn, task_id, self)
+        handle_global_pagefault(get_memory_set, vpn, task_id, self)
     }
 
     fn insert_frame(&mut self, vpn: VPN<Meta>, ppn: PPN<Meta>, task_id: usize, frame: FrameTracker) {
-        if self.task_id == usize::MAX {
-            self.task_id = task_id;
+        if let Some(vec) = self.queue.get_mut(&task_id) {
+            vec.push_back((ppn, vpn, frame));
+        } else {
+            let mut tmp = ClockQueue::new();
+            tmp.push_back((ppn, vpn, frame));
+            self.queue.insert(task_id, tmp);
         }
-        assert!(self.task_id == task_id);
-
-        self.queue.push_back((vpn, frame))
     }
 
-    fn work<F>(&mut self, get_memory_set: &F) -> Vec<(PPN<Meta>, VPN<Meta>, usize)>  
+    fn work<F>(&mut self, get_memory_set: &F, task_id: usize) -> Vec<(PPN<Meta>, VPN<Meta>, usize)>  
         where F: Fn(usize) -> &'static mut AddressSpace<Meta, M> {
-        assert!(self.queue.len() != 0);
-        let memory_set = get_memory_set(self.task_id);
-        loop {
-            if self.ptr >= self.queue.len() {
-                self.ptr = 0;
-            }
-            let (vpn, frame) = &self.queue[self.ptr];
-
-            let (accessed, dirty) = Self::get_accessed_dirty(memory_set, vpn);
-            if accessed || dirty {
-                Self::clear_accessed_and_dirty(memory_set, vpn);
-                self.ptr += 1;
-            } else {
-                let (vpn, _) = self.queue.remove(self.ptr).unwrap();
-                return vec![(PPN::new(0), vpn, 0)];
-            }
+        if !frame_check() {
+            let memory_set = get_memory_set(task_id);
+            let queue = self.queue.get_mut(&task_id).unwrap();
+            assert!(queue.len() != 0);
+            let item = queue.work_improve(memory_set);
+            vec![(item.0, item.1, task_id)]
+        } else {
+            vec![]
         }
     }
 
-    fn clear_frames(&mut self, task_id: usize) {}
+    fn clear_frames(&mut self, task_id: usize) {
+        self.queue.remove(&task_id);
+    }
 
     fn handle_time_interrupt<F>(&mut self, get_memory_set: &F) 
             where F: Fn(usize) -> &'static mut AddressSpace<Meta, M> {}
--- frame-manage/src/clock_queue.rs
+++ frame-manage/src/clock_queue.rs
@@ -1,10 +1,8 @@
 use alloc::collections::VecDeque;
-use alloc::vec::Vec;
-use alloc::vec;
 use kernel_vm::{AddressSpace, VmMeta, PageManager, VPN, Pte, PPN};
 
 use crate::frame_allocator::FrameTracker;
-use crate::ACCESS_FLAG;
+use crate::{ACCESS_FLAG, DIRTY_FLAG};
 
 
 pub struct ClockQueue<Meta: VmMeta> {
@@ -26,6 +24,16 @@ impl<Meta: VmMeta> ClockQueue<Meta> {
     fn clear_accessed<M: PageManager<Meta>>(memory_set: &mut AddressSpace<Meta, M>, vpn: &VPN<Meta>) {
         memory_set.clear_accessed(*vpn);
     }
+
+    fn get_accessed_dirty<M: PageManager<Meta>>(memory_set: &mut AddressSpace<Meta, M>, vpn: &VPN<Meta>) -> (bool, bool) {
+        let pte = Self::get_pte(memory_set, vpn).unwrap();
+        let flags = pte.flags().val();
+        ((flags & ACCESS_FLAG) != 0, (flags & DIRTY_FLAG) != 0)
+    }
+
+    fn clear_accessed_and_dirty<M: PageManager<Meta>>(memory_set: &mut AddressSpace<Meta, M>, vpn: &VPN<Meta>) {
+        memory_set.clear_accessed_and_dirty(*vpn);
+    }
 }
 
 impl<Meta: VmMeta> ClockQueue<Meta> {
@@ -41,7 +49,7 @@ impl<Meta: VmMeta> ClockQueue<Meta> {
         self.inner.len()
     }
 
-    pub fn work<M: PageManager<Meta>>(&mut self, memory_set: &mut AddressSpace<Meta, M>) -> Vec<(PPN<Meta>, VPN<Meta>, usize)> {
+    pub fn work<M: PageManager<Meta>>(&mut self, memory_set: &mut AddressSpace<Meta, M>) -> (PPN<Meta>, VPN<Meta>) {
         loop {
             if self.ptr >= self.inner.len() {
                 self.ptr = 0;
@@ -52,7 +60,25 @@ impl<Meta: VmMeta> ClockQueue<Meta> {
                 self.ptr += 1;
             } else {
                 let (ppn, vpn, _) = self.inner.remove(self.ptr).unwrap();
-                return vec![(ppn, vpn, 0)];
+                return (ppn, vpn);
+            }
+        }
+    }
+
+    pub fn work_improve<M: PageManager<Meta>>(&mut self, memory_set: &mut AddressSpace<Meta, M>) -> (PPN<Meta>, VPN<Meta>) {
+        loop {
+            if self.ptr >= self.inner.len() {
+                self.ptr = 0;
+            }
+            let (ppn, vpn, frame) = &self.inner[self.ptr];
+
+            let (accessed, dirty) = Self::get_accessed_dirty(memory_set, vpn);
+            if accessed || dirty {
+                Self::clear_accessed_and_dirty(memory_set, vpn);
+                self.ptr += 1;
+            } else {
+                let (ppn, vpn, _) = self.inner.remove(self.ptr).unwrap();
+                return (ppn, vpn);
             }
         }
     }
